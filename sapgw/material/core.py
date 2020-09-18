d557b856d8d18c083642c69330fe7e51c8dfae71
nuc <nuc@milano1.it>
2020-09-18 18:46:30 +0200

    wip texst, lint code

--- sapgw/material/core.py
+++ sapgw/material/core.py
@@ -15,11 +15,12 @@ import time
 
 from sapgw.session import Session, parseApiError
 
+
 class Material(object):
     """
     SAPGW Materials.
     """
-    
+
     def __init__(self, profile_name=None):
         """
         Init Material class.
@@ -28,19 +29,19 @@ class Material(object):
         s = Session(profile_name)
         host = s.config.get('sapgw_host')
         self.host = host
-        self.s = s   
+        self.s = s
 
-    def getMaterialAna(self, material_id:str):
+    def getMaterialAna(self, material_id: str):
         """
         Anagrafica materiale.
         """
         logging.info(f'Reading material {material_id} ana...')
         payload = {
-            '$format' : 'json',
-            '$expand' : 'ToDescriptions'
+            '$format': 'json',
+            '$expand': 'ToDescriptions'
         }
         rq = f"{self.host}/ZMATERIAL_GET_ALL_SU_SRV/zmaterial_client_dataSet(Material='{material_id}')"
-        agent=self.s.getAgent()
+        agent = self.s.getAgent()
         r = agent.get(rq, params=payload)
         if 200 != r.status_code:
             parseApiError(r)
@@ -48,16 +49,16 @@ class Material(object):
         material_ana = r.text
         return material_ana
 
-    def getMaterialClass(self, material_id:str):
+    def getMaterialClass(self, material_id: str):
         """
         Classificazione materiale.
         """
         logging.info(f'Reading material {material_id} class...')
         payload = {
-            '$format' : 'json'
+            '$format': 'json'
         }
         rq = f"{self.host}/ZMATERIAL_CLASSIFICATION_SU_SRV/z_material_classSet(Material='{material_id}')/ToClassification"
-        agent=self.s.getAgent()
+        agent = self.s.getAgent()
         r = agent.get(rq, params=payload)
         if 200 != r.status_code:
             parseApiError(r)
@@ -65,17 +66,18 @@ class Material(object):
         material_class = r.text
         return material_class
 
-    def getMaterialStock(self, material_id:str, plant=None):
+    def getMaterialStock(self, material_id: str, plant=None):
         """
         Stock disponibile.
         """
         logging.info(f'Reading material {material_id} stock...')
         payload = {
-            '$format' : 'json'
+            '$format': 'json'
         }
-        if plant:payload['$filter']=f"Plant eq '{plant}'"
-        rq=f"{self.host}/ZMATERIAL_GET_STOCK_SRV/zmaterial_stockSet('{material_id}')/To_Get_Stock"
-        agent=self.s.getAgent()
+        if plant:
+            payload['$filter'] = f"Plant eq '{plant}'"
+        rq = f"{self.host}/ZMATERIAL_GET_STOCK_SRV/zmaterial_stockSet('{material_id}')/To_Get_Stock"
+        agent = self.s.getAgent()
         r = agent.get(rq, params=payload)
         if 200 != r.status_code:
             parseApiError(r)
--- test/material.py
+++ test/material.py
@@ -13,27 +13,6 @@ import logging
 
 from sapgw.material.core import Material
 
-class test():
-    """ test class """
-    def __init__(self, profile_name=None):
-        logging.debug('Init test')
-        self.m = Material(profile_name)
-
-    def fullMaterial(self, material_id:str):
-        """ test ana + class """
-        logging.debug(f'test full material {material_id}')
-        mana = self.m.getMaterialAna(material_id)
-        logging.info(mana)
-        mcls = self.m.getMaterialClass(material_id)
-        logging.info(mcls)
-        return True
-    
-    def stock(self, material_id):
-        """ test stock """
-        logging.debug(f'test stock {material_id}')
-        mstock = self.m.getMaterialStock(material_id)
-        logging.info(mstock)
-        return True
 
 def main(args):
     """ start testing """
@@ -41,19 +20,32 @@ def main(args):
     if args.verbose:
         logging.getLogger().setLevel(logging.DEBUG)
     logging.debug(f'Init {__file__}')
-    t = test(profile_name=args.profile)
-    for atr in args.test:
-        if hasattr(t, atr):getattr(t, atr)()
+
+    m = Material(args.profile)
+    mana = m.getMaterialAna(args.material)
+    if 200 != mana.status_code:
+        print()
+    print(type(mana.json()), type(mana.text))
+    #
+    # mcls = m.getMaterialClass(args.material)
+    # print(mcls)
+    # mstock = m.getMaterialStock(args.material)
+    # print(mstock)
     return
-    
+
+
 def parse_args():
     """Parse the args from main."""
     import argparse
     parser = argparse.ArgumentParser(description='Testing support')
+    parser.add_argument("--material", type=str,
+                        help='Material id', required=True)
     parser.add_argument("--profile", type=str, help='Use specific profile env')
-    parser.add_argument("-t", "--test", nargs='+', help='What can I do for you?', required=True)
-    parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+    parser.add_argument("-opt", "--option", help='Test option')
+    parser.add_argument("-v", "--verbose",
+                        help="increase output verbosity", action="store_true")
     return parser.parse_args()
+
 
 if __name__ == '__main__':
     args = parse_args()
